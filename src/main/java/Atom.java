d493e3763b95302afa47811d16874081c28ce2d1
pleasechangeluis <luis.angel.martinez.canon@gmail.com>
2025-11-16 21:47:49 -0500

    Atom and DisplayAtom Made
    started working on a few of the model classes

---
+++ src/main/java/Atom.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public interface Atom {
+    void receive(int energy, int direction);
+    double display();
+    void disperse();
+}
---
+++ src/main/java/DisplayAtom.java
@@ -0,0 +1,37 @@
+package main.java;
+
+import java.lang.reflect.Array;
+
+public class DisplayAtom implements Atom {
+    double Energy;
+    int direction;
+    Atom[] NearAtoms = new Atom[8];
+
+    public DisplayAtom (Atom[] nearByAtoms){
+        Energy = 0;
+        int counter = 0;
+        for(Atom cAtom: nearByAtoms){
+            NearAtoms[counter] = cAtom;
+            counter ++;
+        }
+    }
+    public void receive(int energy, int direction){
+        this.Energy = energy;
+        this.direction = direction;
+
+    }
+    public double display(){
+        return this.Energy;
+    }
+    public void disperse(){
+        // Depending on directions value (we can decide on encoding later, leaning towards numbers 1-9 being a direction) this atom should .recieve 
+        // the correct atoms))
+        switch (this.direction){
+            case (0):
+            break;
+            default:
+            break;
+        }
+    } 
+    
+}
